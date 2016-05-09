101aeec9afc699d9e23ae2245be628ecf6ca00a0
Matt Pua <matt.pua@utoronto.ca>
2016-05-09 13:27:32 -0400

    Updates

--- bundle.js
+++ bundle.js
@@ -90308,14 +90308,14 @@
 	    var el = this.el;
 
 	    // Set color using raycaster parent color.
-	    el.addEventListener('cursor-click', function (evt) {
-	      console.log(evt);
-	      document.getElementsByTagName('audio')[0].play();
-	    });
+	    /*    el.addEventListener('cursor-click', function (evt) {
+	          console.log(evt);
+	          document.getElementsByTagName('audio')[0].play();
+	        });*/
 
 	    window.addEventListener('touchstart', function () {
 	      document.getElementsByTagName('audio')[0].play();
-	      document.getElementsByTagName('audio')[0].pause();
+	      // document.getElementsByTagName('audio')[0].pause();
 	    });
 	  }
 	});
--- js/aframe_components/cursor-interaction.js
+++ js/aframe_components/cursor-interaction.js
@@ -3,14 +3,14 @@ AFRAME.registerComponent('cursor-interaction', {
     var el = this.el;
 
     // Set color using raycaster parent color.
-    el.addEventListener('cursor-click', function (evt) {
+/*    el.addEventListener('cursor-click', function (evt) {
       console.log(evt);
       document.getElementsByTagName('audio')[0].play();
-    });
+    });*/
 
     window.addEventListener('touchstart', function(){
       document.getElementsByTagName('audio')[0].play();
-      document.getElementsByTagName('audio')[0].pause();
+      // document.getElementsByTagName('audio')[0].pause();
     });
 
 
