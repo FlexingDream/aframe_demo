8a425167dc62f4b0f8a07cbe30837433b4cffc43
Matt Pua <matt.pua@utoronto.ca>
2016-05-24 10:08:17 -0400

    Got the basic hand and scene

--- src/js/scenes/port_rob.js
+++ src/js/scenes/port_rob.js
@@ -40,9 +40,10 @@ class PortRob extends React.Component{
   getModels(){
     return(
       <Entity>
-        <a-asset-item id="moon" src="../3d_models/moon.dae"></a-asset-item>
-        <a-asset-item id="terrain" src="../3d_models/terrain.dae"></a-asset-item>
-        <a-asset-item id="hand" src="../3d_models/hand.dae"></a-asset-item>
+        <a-asset-item id="moon-asset" src="../3d_models/moon.dae"></a-asset-item>
+        <a-asset-item id="terrain-asset" src="../3d_models/terrain.dae"></a-asset-item>
+        <a-asset-item id="hand-asset" src="../3d_models/hand.dae"></a-asset-item>
+        <a-asset-item id="valley-asset" src="../3d_models/valley.dae"></a-asset-item>
       </Entity>
     );
   }
@@ -59,31 +60,67 @@ class PortRob extends React.Component{
       );
   }
 
+
+  componentDidMount(){
+    console.log('test');
+    this.captureSongStart();
+  }
+
+  startSong(){
+    document.getElementById('scene').removeEventListener('song_loaded',this.startSong,false);
+    document.querySelector('#camera').emit('start_song');
+    document.querySelector('#moon').emit('start_song');
+  }
+
+  captureSongStart(){
+    document.getElementById('scene').addEventListener('song_loaded',this.startSong,false);
+  }
+
   render(){
     return(
-    <Scene stats fog={{type: 'linear', near:50,color: '#1D2327'}}>
+    <Scene id="scene" stats fog={{type: 'linear', near:50,color: '#1D2327'}}>
       {this.getAssets()}
-      <Camera position={[0,1,0]} wasd-controls={{enabled: false}}>
+      <Camera id="camera" position={[0,10,0]} wasd-controls={{enabled: false}} >
         <Cursor />
-        <Animation attribute="position" to="0 0 -100" dur="80000" ease="ease-in-out" />
-        <Entity collada-model="#hand" scale="0.1 0.1 0.1" position="0 -1.4 -1.5" rotation="0 105 15">
-          <Animation attribute="position" to="0 -1.6 -1.5" dur="10000" repeat="indefinite" direction="alternate" from="0 -1.4 -1.5" ease="ease-in-out"/>
-        </Entity>
+        <Animation attribute="position" to="0 0 -100" dur="80000" ease="ease-in-out" begin="start_song"/>
+        <Hand/>
+
       </Camera>
       <Entity light={{type: 'directional', intensity: 0.9}} position="0 100 0"/>
       <Audio  audioSrc={this.state.song} frequencySize={this.props.frequencySize} refreshRate={this.props.refreshRate} shouldUpdateFrequencies={this.shouldUpdateFrequencies.bind(this)}/>
       <Sky color='#0B141A'/>
-      <Entity collada-model='#moon' position="0 0 -500" scale="75 75 75" rotation="180 180 140">
-        <Animation attribute="position" to="0 100 -500" dur="30000" ease="ease-in-out" />
-        <Entity light={{type: 'point', intensity:0.8, distance: 150}}>
-          <Animation attribute="position" to="0 100 -400" dur="30000" ease="ease-in-out"/>
-        </Entity>
+      <Entity id="moon" collada-model='#moon-asset' position="0 0 -500" scale="75 75 75" rotation="180 180 140">
+        <Animation attribute="position" to="0 100 -500" dur="60000" ease="ease-in-out" begin="start_song"/>
       </Entity>
-      <Entity collada-model="#terrain" position="0 -5 0" rotation="0 270 0" rotation="0 90 0"/>
+      <Entity collada-model="#terrain-asset" position="0 -5 0" rotation="0 90 0"/>
+      <Valley/>
       <Fog/>
     </Scene>);
   }
+}
 
+class Hand extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(        
+      <Entity collada-model="#hand-asset" scale="0.1 0.1 0.1" position="0 -1.4 -1.5" rotation="0 105 15">
+        <Animation attribute="position" to="0 -1.6 -1.5" dur="10000" repeat="indefinite" direction="alternate" from="0 -1.4 -1.5" ease="ease-in-out"/>
+      </Entity>
+  );
+  }
+}
+
+class Valley extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (<Entity id="valley" collada-model="#valley-asset" position="0 -5 -300" rotation="0 180 0" visible="false" />);
+  }
 }
 
 class Fog extends React.Component{
