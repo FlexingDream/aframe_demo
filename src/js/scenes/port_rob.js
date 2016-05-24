e8412edf6de4b1dade474bbed6003755616c3f65
Matt Pua <matt.pua@utoronto.ca>
2016-05-24 15:44:34 -0400

    better scene

--- src/js/scenes/port_rob.js
+++ src/js/scenes/port_rob.js
@@ -13,6 +13,7 @@ import Cursor from '../components/Cursor';
 import Audio from '../components/Audio';
 import Sky from '../components/Sky';
 import '../aframe_components/entity-generator';
+import 'aframe-text-component';
 // import '../aframe_components/three-model.js';
 AframeExtras.loaders.registerAll();
 
@@ -47,6 +48,7 @@ class PortRob extends React.Component{
         <a-asset-item id="terrain-asset" src="../3d_models/terrain.dae"></a-asset-item>
         <a-asset-item id="hand-asset" src="../3d_models/hand.dae"></a-asset-item>
         <a-asset-item id="valley-asset" src="../3d_models/valley.dae"></a-asset-item>
+        <a-asset-item id="gradient-sky-asset" src="../3d_models/gradient-sky.dae"></a-asset-item>
       </Entity>
     );
   }
@@ -65,41 +67,135 @@ class PortRob extends React.Component{
 
 
   componentDidMount(){
-    console.log('test');
     this.captureSongStart();
   }
 
   startSong(){
     document.getElementById('scene').removeEventListener('song_loaded',this.startSong,false);
-    document.querySelector('#camera').emit('start_song');
-    document.querySelector('#moon').emit('start_song');
+
+    document.getElementById('intro').emit('start_intro');
+  }
+
+  startIntro(){
+    document.getElementById('intro').removeEventListener('start_intro',this.startIntro,false);
+
+    document.getElementsByClassName("intro-text")[0].emit("reveal");
+    setTimeout(function(){
+      document.getElementsByClassName("intro-text")[1].emit("reveal");
+      setTimeout(function(){
+        document.getElementsByClassName("intro-text")[2].emit("reveal");
+        setTimeout(function(){
+          document.getElementById('part_1').emit('start_part1');
+        },1000);
+      },600);
+    },2000);
+  }
+
+  startPart1(){
+    document.getElementById('part_1').removeEventListener('start_part1',this.startPart1,false);
+    $("#intro").remove();
+
+    setTimeout(function(){
+      document.getElementById("hand").emit("show_hand");
+      setTimeout(function(){
+        document.getElementById("hand").emit("rotate_hand");
+      },4000);
+    },4000);
+
+    setTimeout(function(){
+      // document.getElementById("world_light").emit("dim_light");
+      document.getElementById("part_2").emit("start_part2");
+    },40000);
+  }
+
+  startPart2(){
+    document.getElementById('part_2').removeEventListener('start_part2',this.startPart2,false);
+    // $("#part_1").remove();
+    document.getElementById('part_2').setAttribute('visible',true);
+    document.getElementById('moon').emit("move_moon");
+    document.getElementById('world_light').emit('dim_light');
+    document.getElementById('moon_light').emit('brighten_light');
   }
 
   captureSongStart(){
-    document.getElementById('scene').addEventListener('song_loaded',this.startSong,false);
+    document.getElementById('scene').addEventListener('song_loaded',this.startSong.bind(this),false);
+    document.getElementById('intro').addEventListener('start_intro',this.startIntro.bind(this),false);
+    document.getElementById('part_1').addEventListener('start_part1',this.startPart1.bind(this),false);
+    document.getElementById('part_2').addEventListener('start_part2',this.startPart2.bind(this),false);
   }
 
   render(){
     return(
     <Scene id="scene" stats fog={{type: 'linear', near:50,color: '#1D2327'}}>
       {this.getAssets()}
-      <Camera id="camera" position={[0,15,0]} wasd-controls={{enabled: false}} >
+      <Camera id="camera" position={[0,10,0]} wasd-controls={{enabled: false}} >
         <Cursor />
-        <Animation attribute="position" to="0 0 -100" dur="80000" ease="ease-in-out" begin="start_song"/>
-        <Entity light={{type: 'point'}} position="0 0 0" />
+        <Animation attribute="position" to="0 0 -200" dur="160000" ease="ease-in-out" begin=""/>
         <Hand/>
+        <Entity id="world_light" light={{type: 'point', distance: 200, decay: 2}}>
+          <Animation attribute="light.intensity" dur="10000" to="0.4" begin="dim_light"/>
+        </Entity>
       </Camera>
-      <Entity light={{type: 'point', intensity: 0.8, distance: 400}} position="0 200 0"/>
       <Audio  audioSrc={this.state.song} frequencySize={this.props.frequencySize} refreshRate={this.props.refreshRate} shouldUpdateFrequencies={this.shouldUpdateFrequencies.bind(this)}/>
-      <Sky color='#0B141A'/>
-      <Entity id="moon" collada-model='#moon-asset' position="-25 -40 -500" scale="75 75 75" rotation="180 180 140">
-        <Animation attribute="position" to="-25 100 -500" dur="60000" ease="ease-in-out" begin="start_song"/>
-      </Entity>
-      <Entity collada-model="#terrain-asset" position="0 -5 0" rotation="0 90 0"/>
-      <Valley/>
-      <Fog/>
-      <Stars/>
+      <Intro/>
+      <Part1/>
+      <Part2/>
     </Scene>);
+  }
+}
+
+class Intro extends React.Component{
+  render(){
+    return(
+    <Entity id="intro">
+      <Sky color="black"/>
+      <Entity position="-30 50 -50" look-at='[camera]'>
+        <Entity class="intro-text" text={{text: "IS ANYONE THERE?",height: 1, size: 5}}  material={{color:'white'}} visible="false">
+          <Animation attribute="visible" dur="400" to="true" begin="reveal"/>
+        </Entity>
+        <Entity class="intro-text" text={{text: "OH - ",height: 1, size: 5}}  position="0 -10 0" material={{color:'white'}} visible="false">
+          <Animation attribute="visible" dur="400" to="true" begin="reveal"/>
+        </Entity>
+        <Entity class="intro-text" text={{text: "HI!",height: 1, size: 5}}  position="0 -20 0" material={{color:'white'}} visible="false">
+          <Animation attribute="visible" dur="400" to="true" begin="reveal"/>
+        </Entity>
+        <Entity class="intro-text" text={{text: "Porter Robinson - Sad Machine",height: 1, size: 8}}  position="5 20 0" material={{color:'white'}}/>
+      </Entity>
+    </Entity>
+    );
+  }
+}
+
+class Part1 extends React.Component{
+  render(){
+    return(
+      <Entity id="part_1" visible="false">
+        <Sky color='#0B141A'/>
+        <Animation attribute="visible" to="true" begin="start_part1"/>
+        <Entity collada-model="#terrain-asset" position="0 -5 0" rotation="0 0 0"/>
+        <Fog/>
+
+      </Entity>
+    );
+  }
+}
+
+class Part2 extends React.Component{
+        // <Entity light={{type: 'point', intensity: 0.8, distance: 400}} position="0 200 0"/>
+
+  render(){
+    return(
+      <Entity id="part_2" visible="false">
+        <Animation attribute="position" to="0 0 -800" dur="120000" ease="ease-in-out" begin="move_moon"/>
+        <Entity id="moon" collada-model='#moon-asset' position="-25 -40 -500" scale="75 75 75" rotation="180 180 140">
+          <Animation attribute="position" to="-25 150 -500" dur="60000" ease="ease-in-out" begin="move_moon"/>
+        </Entity>
+        <Entity light={{type: 'point', distance: 300}} position="-25 -50 -480">
+          <Animation attribute="light.intensity" to="1" from="0" begin="brighten_light" dur="60000"/>
+        </Entity>
+        <Stars/>
+      </Entity>
+    );
   }
 }
 
@@ -119,10 +215,13 @@ class Hand extends React.Component{
 
   render(){
     return(        
-      <Entity collada-model="#hand-asset" scale="0.1 0.1 0.1" position="0 -1.4 -1.5" rotation="0 105 15">
+      <Entity id="hand" collada-model="#hand-asset" scale="0.1 0.1 0.1" position="0 -1.4 -1.5" rotation="0 105 15" visible="false">
+        <Animation attribute="position" from="0 -2.5 -1.5" to="0 -1.6 -1.5" dur="8000" ease="linear" begin="show_hand"/>
+        <Animation attribute="visible" to="true" dur="8000" ease="linear" begin="show_hand"/>
         <Animation attribute="position" to="0 -1.6 -1.5" dur="10000" repeat="indefinite" direction="alternate" from="0 -1.4 -1.5" ease="ease-in-out"/>
+        <Animation attribute="rotation" to ="45 270 90" from="0 105 15" direction="alternate" repeat="1" dur="15000" begin="rotate_hand" ease="linear"/>
       </Entity>
-  );
+    );
   }
 }
 
@@ -141,6 +240,7 @@ class Fog extends React.Component{
 
     return(
       <Entity>
+        <Animation attribute="position" to="0 0 -800" dur="120000"/>
         <Entity geometry={{primitive: 'box', width: 800, height: 100, depth: 2}} position="0 20 -480" material={{color: '#B38AAA', opacity: 0.2}}/>
         <Entity geometry={{primitive: 'box', width: 800, height: 60, depth: 2}} position="0 10 -478" material={{color: '#673D68', opacity: 0.4}}/>
         <Entity geometry={{primitive: 'box', width: 800, height: 100, depth: 2}} position="0 10 -476" material={{color: '#A86B9F', opacity: 0.6}}/>
