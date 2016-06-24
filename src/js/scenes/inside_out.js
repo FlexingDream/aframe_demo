91e321b26e4e942a7e6b0016c3baa846de221d8f
Gavin Ching <gavinchingy@gmail.com>
2016-06-24 10:07:07 -0400

    Incorporate loading

--- src/js/scenes/inside_out.js
+++ src/js/scenes/inside_out.js
@@ -6,25 +6,47 @@ import {Scene, Entity} from 'aframe-react';
 import ReactDOM from 'react-dom';
 import SkyToOcean from '../components/_SkyToOcean';
 import Audio from '../components/Audio';
+import Loading from '../components/_Loading';
 
 
 class InsideOut extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      stage: 0
+    }
   }
-  componentDidMount(){
-    $(".audio-player").data("node").start();
+
+  nextScene(){
+    let currStage = this.state.stage;
+    if (currStage == 0){
+      var node = $(".audio-player").data("node");
+      node.start(0);
+    }
+    let nextStage = currStage+1;
+    let includeFog = false;
+    this.setState({stage: nextStage,includeFog: includeFog});
   }
+
+  getScene(){
+    if (this.state.stage == 0)
+      return <Loading nextScene={this.nextScene.bind(this)}/>;
+    else
+      return <SkyToOcean/>;
+  }
+
   render(){
     return(
       <Scene stats fog id="scene">
+
         <a-assets>
           <Entity>
             <a-mixin id="laser" geometry="primitive: box; depth: 0.5; width: 0.5;" material="opacity:0.3;shader:flat;"></a-mixin>
           </Entity>
         </a-assets>
-        <Audio  audioSrc={'audio/lion_wild.mp3'} shouldUpdateFrequencies="false" shouldPlay={true}/>
-        <SkyToOcean/>
+
+        <Audio  audioSrc={'audio/inside_out.mp3'} shouldUpdateFrequencies="false" shouldPlay={true}/>
+        {this.getScene()}
 
 
       </Scene>
